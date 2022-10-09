dc340f2182970d575d3da58eb63dcee9b738be29
AlexRicartR <alex.ricart@outlook.com>
2022-10-09 13:54:29 +0200

    Adding all remaining exercises

--- js/main.js
+++ js/main.js
@@ -161,3 +161,39 @@ aunque le queden intentos). */
         //                 i++
         //         }
         // }
+
+/*13rd Exercise
+Crea una aplicación llamada CalculadoraInversa, nos pedirá 2 operandos (int) y
+un signo aritmético (String), según este último se realizará la operación
+correspondiente. Al final mostrara el resultado en un cuadro de dialogo.
+Los signos aritméticos disponibles son:
++: suma los dos operandos.
+-: resta los operandos.
+*: multiplica los operandos.
+/: divide los operandos, este debe dar un resultado con decimales (double)
+^: 1º operando como base y 2º como exponente.
+%: módulo, resto de la división entre operando1 y operando2.*/
+
+        // let operando1 = parseInt(prompt("Introduce primer operando"));
+        // let operando2 = parseInt(prompt("Introduce segundo operando"));
+        // let simboloarit = (prompt("Introduce uno de los siguientes simbolos aritmeticos: +: suma los dos operandos. ; -: resta los operandos.; *: multiplica los operandos. ; /: divide los operandos, este debe dar un resultado con decimales (double) ; ^: 1º operando como base y 2º como exponente.; %: módulo, resto de la división entre operando1 y operando2."));
+
+        // if (simboloarit==="+") {
+        //         result=operando1+operando2
+        //         alert(result)
+        // } else if ((simboloarit==="-")) {
+        //         result=operando1-operando2
+        //         alert(result)
+        // } else if ((simboloarit==="*")) {
+        //         result=operando1*operando2
+        //         alert(result)
+        // } else if ((simboloarit==="/")) {
+        //         result=operando1/operando2
+        //         alert(result)
+        // } else if ((simboloarit==="^")) {
+        //         result=operando1^operando2
+        //         alert(result)
+        // } else if ((simboloarit==="%")) {
+        //         result=operando1%operando2
+        //         alert(result)
+        // }
